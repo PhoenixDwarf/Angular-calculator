f3624a9048d5aa757a07bf30782a639dff19a64b
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-29 14:23:02 -0500

    feat: refactor calculator button component and update usage in calculator view

--- src/app/calculator/components/calculator-button/calculator-button.ts
+++ src/app/calculator/components/calculator-button/calculator-button.ts
@@ -1,11 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { booleanAttribute, ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './calculator-button.html',
+  styleUrl: './calculator-button.css',
+  // New way of doing host bindings, no need for decorator and additional code!
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'doubleSize()',
   },
 })
-export class CalculatorButton {}
+export class CalculatorButton {
+  // booleanAttribute
+  // Transforms a value (typically a string) to a boolean. Intended to be used as a transform function of an input.
+  public command = input(false, { transform: booleanAttribute });
+  public firstRow = input(false, { transform: booleanAttribute });
+  public division = input(false, { transform: booleanAttribute });
+  public doubleSize = input(false, { transform: booleanAttribute });
+}
