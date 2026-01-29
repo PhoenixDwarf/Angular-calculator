46651ce52fbaaacc5375a7421a3e51eccf3088e2
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-29 11:57:23 -0500

    feat: update routing and create calculator view
    - Added a new route for the calculator view and set it as the default redirect.
    - Changed the application title from "ProCalculator" to "Zoneless Calculator".
    - Created the HTML and TypeScript files for the calculator view component.
    - Updated the index.html to reflect the new title.
    - Configured TypeScript paths for easier imports.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('pro-calculator');
+  protected readonly title = signal('zoneless-calculator');
 }
