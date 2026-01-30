f51c76c14d8a151ccd64c04a8854d4f8c0bf092c
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-30 11:11:10 -0500

    feat: update app component tests to include router-outlet rendering and CSS class validation

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,30 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pro-calculator');
+
+    const routerOutlet = compiled.querySelector('router-outlet');
+
+    expect(routerOutlet).toBeTruthy();
+  });
+
+  it('should render router-outlet parent div with must-have css classes', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const divElement = compiled.querySelector('div');
+
+    expect(divElement).toBeTruthy();
+
+    const mustHaveClasses =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' ',
+      );
+
+    const hasAllClasses = mustHaveClasses.every((cls) => divElement?.classList.contains(cls));
+
+    expect(hasAllClasses).toBe(true);
   });
 });
